b69bcaac73b1448e4b1c4fbc5ae2a790d266ed76
martin lowe <martinlowe673@gmail.com>
2020-10-19 20:21:22 +0100

    played with routes should work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  resources :gardens do
+    resources :plants, only: [ :create ]
+  end
+
+  resources :plants, only: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
